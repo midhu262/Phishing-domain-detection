9e92363157bdde17631b4874272cbf4cf0a67294
User <user@localhost.localdomain>
2021-09-29 13:46:00 -0400

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='predict wheather a domain is malicious or not.',
+    author='midhu262',
+    license='MIT',
+)
